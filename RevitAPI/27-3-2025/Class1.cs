8ff606fe831768740bb16ead8f6a7c8174eb54e1
Nguyễn Quang Minh-67TH2-0036967 <0036967@st.huce.edu.vn>
2025-03-27 13:30:33 +0700

    ok

---
+++ RevitAPI/27-3-2025/Class1.cs
@@ -0,0 +1,26 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _002_HelloWorld
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class A002_SelectElement : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            ICollection<Element> selectElements = (ICollection<Element>)uidoc.Selection.GetElementIds();
+
+            int totalCount = selectElements.Count;
+            MessageBox.Show("Ban da chon" + totalCount.ToString() + "elements" ;
+            return Result.Succeeded;
+        }
+    }
+}
